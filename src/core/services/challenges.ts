eae705f5c94b396a4c36aa9f3473bc732b7ac404
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2020-11-21 23:07:47 +0900

    add api requests

--- src/core/services/challenges.ts
+++ src/core/services/challenges.ts
@@ -20,9 +20,44 @@ export async function getChallenges() {
     return null;
   }
 
-  if (result.status !== 200) {
-    return null;
-  }
-
   return result.data;
+}
+
+type AddChallenge = Pick<Challenge, 'name'> & {
+  tagIds: Tag['id'][];
+}
+
+export async function addChallenge(challenge: AddChallenge) {
+  try {
+    await axios.post<Challenge>(`${BASE_URL}/challenges`, {
+      data: challenge,
+    });
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
+type UpdateChallenge = Pick<Challenge, 'id' | 'name'> & {
+  tagIds: Tag['id'][];
+}
+
+export async function updateChallenge(challenge: UpdateChallenge) {
+  try {
+    await axios.put<Challenge>(`${BASE_URL}/challenges`, {
+      data: challenge,
+    });
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
+export async function removeChallenge(id: Challenge['id']) {
+  try {
+    await axios.delete<Challenge>(`${BASE_URL}/challenges/${id}`);
+  } catch (error) {
+    return false;
+  }
+  return true;
 }
--- src/core/services/sentences.ts
+++ src/core/services/sentences.ts
@@ -1,28 +1,55 @@
 import axios from 'axios';
 import { BASE_URL } from './request';
+import { Tag } from './tags';
 
 export type Sentence = {
   id: string;
   en: string;
   ja: string[];
+  tags: Tag[];
 }
 
 export async function getAll() {
   const result = await axios.get<Sentence[]>(`${BASE_URL}/sentences`);
-  if (result.status !== 200) {
-    return null;
-  }
 
   return result.data;
 }
 
-export type SentenceAdd = Omit<Sentence, 'id'>;
+type SentenceAdd = Omit<Sentence, 'id' | 'tags'> & {
+  tagIds: Tag['id'][];
+};
 
 export async function add(sentence: SentenceAdd) {
-  const result = await axios.post(`${BASE_URL}/sentences`, sentence);
-  if (result.status !== 200) {
-    return null;
+  try {
+    await axios.post(`${BASE_URL}/sentences`, sentence);
+  } catch (error) {
+    return false;
+  }
+  return true;
+}
+
+type SentenceUpdate = Omit<Sentence, 'tags'> & {
+  tagIds: Tag['id'][];
+};
+
+export async function update(sentence: SentenceUpdate) {
+  try {
+    await axios.put(`${BASE_URL}/sentences`, {
+      data: sentence,
+    });
+  } catch (error) {
+    return false;
   }
 
-  return result.data;
+  return true;
+}
+
+export async function remove(id: Sentence['id']) {
+  try {
+    await axios.delete(`${BASE_URL}/sentences/${id}`);
+  } catch (error) {
+    return false;
+  }
+
+  return true;
 }
